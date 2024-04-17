c43dd7801c9b82c7c5aba1ad28cdc7d261df1f03
xiu7987 <ib30583239@126.com>
2024-04-17 19:57:14 +0800

    4月17日练习代码

---
+++ test4-17/test4-17/test4-17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int i = 0;
+//	int v = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (v = 1; v <= i; v++)
+//			printf("%dx%d = %d  ", i, v, i * v);
+//		printf("\n");
+//	}
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
