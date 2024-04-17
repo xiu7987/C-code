9bad20b0e348cafceb011c35a39ed0f6336c0950
xiu7987 <ib30583239@126.com>
2024-04-17 20:55:48 +0800

    4月17日练习代码

---
+++ test4-17/test4-17/test4-17.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int i = 0;
+//	int v = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		for (v = 1; v <= i; v++)
+//			printf("%dx%d = %d  ", i, v, i * v);
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf_s ("%d", &n);
+//	if (n % 4 == 0 && n % 100 != 0 || n % 400 == 0)
+//	{
+//		printf("%d是闰年", n);
+//	}
+//	else printf("%d不是闰年", n);
+//	return 0;
+//}
