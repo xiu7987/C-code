2572b30431b1da6efac3ea2abd24320280512c68
xiu7987 <ib30583239@126.com>
2024-06-02 18:02:27 +0800

    6-01代码练习（续）

---
+++ test6-01(course)/test6-01(course)/supplement.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//int main()
+//{
+//	return 0;
+//}
+
+//int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+//{
+//    for (int i = 0; i < numsSize - 1; i++)
+//    {
+//        for (int j = i + 1; j < numsSize; j++)
+//        {
+//            if ((nums[i] + nums[j]) == target)
+//            {
+//                int* arr = (int*)malloc(sizeof(int) * 2);
+//                arr[0] = i;
+//                arr[1] = j;
+//                *returnSize = 2;
+//                return arr;
+//            }
+//        }
+//    }
+//    return 0;
+//}
